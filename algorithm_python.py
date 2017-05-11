3aba4cf3dce4e86ea9ca1573423a23dd23e7d7b4
ld2113 <ld2113@ic.ac.uk>
2017-05-11 11:59:36 +0100

    Update algorithm (from jupyter notebook)

--- algorithm_python.py
+++ algorithm_python.py
@@ -1,27 +1,38 @@
+##Summary:
+#Total proteins in Biogrid DB (only human-human interaction):                                                16,109
+#Total number of unique interactions in Biogrid DB (only human-human interaction and no self-interactions): 219,216
+#Total number of interactions in Biogrid DB (only human-human interaction and no self-interactions):        301,448
+#Max possible interactions between 16,109 proteins (excluding self interactions):                       129,741,886
+#Number of elements in adjacency matrix:                                                                259,499,881
+
 from __future__ import print_function
 from __future__ import division
 
-import scipy.sparse as sp
+import scipy.spatial
 import numpy as np
-
 import pandas as pd
 import networkx as nx
-
 import matplotlib.pyplot as plt
+import sklearn
+from sklearn import metrics
+
 from time import time
 import sys
+import copy
 
-t1 = time()
+
 
 np.random.seed(100)
-K = 30
-Kmax = 200
+K = 4
+Kmax = 5
 dimen = 2
+
+t1 = time()
 
 # Read Biogrid data into pandas dataframe
 df = pd.read_table("~/work/BIOGRID-ORGANISM-Homo_sapiens-3.4.147.mitab.txt", engine="c")
 
-# Rename first two columns of dataframe and strip 'entrez gene/locuslink:' from every row
+# Rename first two columns of dataframe
 df = df.rename(columns={'#ID Interactor A': 'ID Interactor A'})
 df['ID Interactor A'] = df['ID Interactor A'].map(lambda x: x.lstrip('entrez gene/locuslink:'))
 df['ID Interactor B'] = df['ID Interactor B'].map(lambda x: x.lstrip('entrez gene/locuslink:'))
@@ -31,6 +42,9 @@ df['ID Interactor B'] = df['ID Interactor B'].map(lambda x: x.lstrip('entrez gen
 df = df[df['Taxid Interactor A'].isin(['taxid:9606'])]
 df = df[df['Taxid Interactor B'].isin(['taxid:9606'])]
 
+# Remove self interactions
+df = df[df['ID Interactor A'] != df['ID Interactor B']]
+
 # Create networkx graph from dataframe (single edge)
 G = nx.from_pandas_dataframe(df, 'ID Interactor A', 'ID Interactor B', create_using=nx.Graph())
 
@@ -39,13 +53,13 @@ N = G.number_of_nodes()
 
 # Calculate shortest pathlengths dictionary in the graph up to a threshold using networkx
 p = nx.all_pairs_shortest_path_length(G,K)
-
 t2 = time()
 
 print("Start until shortest pathlengths:",t2-t1)
 
-# Reformat path lengths to sparse matrix
-dist_df = pd.DataFrame.from_dict(p).replace(0,2)
+# Reformat path lengths to array
+dist_df_orig = pd.DataFrame.from_dict(p)
+dist_df = dist_df_orig.replace(0,2)
 dist_df = dist_df.fillna(0)-Kmax
 dist_df[dist_df == -Kmax] = 0
 dist_mat = dist_df.values
@@ -98,6 +112,7 @@ while vdiffmax > 0.001:
 		vdiff[i] = np.linalg.norm(v[i] - vnew[i],2)
 
 	vdiffmax = max(vdiff)
+	#print(vdiffmax)
 
 	for i in range(dimen):
 		v[i] = vnew[i]
@@ -120,3 +135,59 @@ print("While loop until end:", t4-t3)
 print("Total time:", t4-t1)
 
 print(xvals)
+
+
+# Create list of protein labels corresponding to embedding output and dict to link names to indices and vice versa
+names = np.array(dist_df_orig.columns, dtype=int)
+index_dic = dict(enumerate(names))
+inverse_dic = {v: k for k, v in index_dic.items()}
+
+# Create adjacency df from distance df and convert to 1D
+adj_df = copy.copy(dist_df_orig)
+adj_df[adj_df!=1.0] = 0
+adj_mat = adj_df.values
+indices = np.triu_indices_from(adj_mat,k=1)
+one_d = adj_mat[indices]
+
+# Calculate distances between all points
+distances = scipy.spatial.distance.cdist(xvals,xvals)
+one_d_dist = distances[np.triu_indices_from(distances,k=1)]
+
+# Number of zero distances
+print("Number of zero distances: ",len(one_d[np.nonzero(one_d_dist==0)]))
+
+# Calculate metrics (threshold independent)
+inv_one_d_dist = 1/one_d_dist
+inv_one_d_dist[inv_one_d_dist == np.inf] = 0
+print("ROC AUC: ",sklearn.metrics.roc_auc_score(one_d,inv_one_d_dist))
+print("PRC AUC: ",sklearn.metrics.average_precision_score(one_d,inv_one_d_dist))
+
+# Replace distances with binary labels according to a threshold
+one_d_thresh_ind = one_d_dist > 0.1
+one_d_dist_bin = copy.copy(one_d_dist)
+one_d_dist_bin[one_d_thresh_ind] = 0
+one_d_dist_bin[np.invert(one_d_thresh_ind)] = 1
+
+# Calculate proportion of 1 in embedded labels
+#dict(zip(np.unique(one_d_dist_bin, return_counts=True)[0],np.unique(one_d_dist_bin, return_counts=True)[1]))
+print("Number of predicted interactions at this threshold: ", np.sum(one_d_dist_bin))
+
+# Calculate metrics (threshold dependent)
+
+print("Precision: ",sklearn.metrics.precision_score(one_d,one_d_dist_bin))
+print("Accuracy: ",sklearn.metrics.accuracy_score(one_d,one_d_dist_bin))
+print("Matthews Corr Coeff: ", sklearn.metrics.matthews_corrcoef(one_d,one_d_dist_bin))
+
+# Plot 2D embedding
+plot = plt.scatter(xvals[:,0],xvals[:,1])
+plt.show()
+
+# Plot ROCurve
+roc=sklearn.metrics.roc_curve(one_d,inv_one_d_dist)
+plt.plot(roc[0],roc[1])
+plt.show()
+
+# Plot PRCurve
+prc=sklearn.metrics.precision_recall_curve(one_d,inv_one_d_dist)
+plt.plot(prc[0],prc[1])
+plt.show()
