5f9dbc4b663fce1932084b42ab5c252f48d73938
ld2113 <ld2113@ic.ac.uk>
2017-05-11 16:22:29 +0100

    Construct neural network

--- neural_network.py
+++ neural_network.py
@@ -1,24 +1,48 @@
+from __future__ import print_function
+from __future__ import division
+
 import numpy as np
+import sys
+
+from keras import regularizers
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
-# Generate dummy data
-x_train = np.random.random((1000, 20))
-y_train = np.random.randint(2, size=(1000, 1))
-x_test = np.random.random((100, 20))
-y_test = np.random.randint(2, size=(100, 1))
 
+# Load coordinate and name data
+names = np.load('names.npy')
+print("names", names.shape)
+labels = np.load('labels.npy')
+print("labels", labels.shape)
+coord = np.load('4Dcoordinates.npy')
+
+# Preprocess data (zero centered and between -1 and 1)
+coord -= 0.5
+coord *= 2
+
+print("coord", coord.shape)
+
+sys.exit()
+
+# Setting hyperparameters
+reg = 0.0
+drop = 0.5
+
+# Setting up model
 model = Sequential()
-model.add(Dense(64, input_dim=20, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dense(64, input_dim=4, activation='relu',kernel_regularizer=regularizers.l2(reg)))
+model.add(Dropout(drop))
+model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(reg)))
+model.add(Dropout(drop))
+model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(reg)))
+model.add(Dropout(drop))
 model.add(Dense(1, activation='sigmoid'))
 
+# Compiling the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=100, batch_size=128)
+# Model training
+model.fit(coord, labels, epochs=10, batch_size=128)
 
-score = model.evaluate(x_test, y_test, batch_size=128)
-
-print(score)
+#score = model.evaluate(x_test, y_test, batch_size=128)
+#print(score)
