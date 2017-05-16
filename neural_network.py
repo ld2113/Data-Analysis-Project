0919f87c2523f3afdcc3f3cc58ff950263c4c326
ld2113 <ld2113@ic.ac.uk>
2017-05-16 17:12:29 +0100

    Change weights init

--- neural_network.py
+++ neural_network.py
@@ -20,22 +20,20 @@ coord -= 0.5
 coord *= 2
 
 # Slicing the data
-keep = 100
+keep = 100000
 labels = labels[0:keep]
 coord = coord[0:keep]
 
 print("----Setting up the model----")
 # Setting hyperparameters
-reg = 0.0
-drop = 0.0
+reg = 0.1
+drop = 0.5
 
 # Setting up model
 model = Sequential()
-model.add(Dense(64, input_dim=8, activation='relu',kernel_regularizer=regularizers.l2(reg)))
+model.add(Dense(8, input_dim=8, activation='relu', kernel_regularizer=regularizers.l2(reg), kernel_initializer='he_normal'))
 model.add(Dropout(drop))
-model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(reg)))
-model.add(Dropout(drop))
-model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(reg)))
+model.add(Dense(4, activation='relu', kernel_regularizer=regularizers.l2(reg), kernel_initializer='he_normal'))
 model.add(Dropout(drop))
 model.add(Dense(1, activation='sigmoid'))
 
@@ -44,7 +42,7 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 print("----Model training----")
 # Model training
-model.fit(coord, labels, epochs=1, batch_size=64)
+model.fit(coord, labels, epochs=10, batch_size=64, validation_split=0.25)
 
 #score = model.evaluate(x_test, y_test, batch_size=128)
 #print(score)
