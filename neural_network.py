5a4d49518b91b1ad9eef88b94a14b6af0aafa35b
ld2113 <ld2113@ic.ac.uk>
2017-05-16 13:24:21 +0100

    First version of NN

--- neural_network.py
+++ neural_network.py
@@ -8,29 +8,30 @@ from keras import regularizers
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
-
+print("----Loading data----")
 # Load coordinate and name data
-names = np.load('names.npy')
-print("names", names.shape)
-labels = np.load('labels.npy')
-print("labels", labels.shape)
-coord = np.load('4Dcoordinates.npy')
+#names = np.load('arrays/names.npy')
+#print("names", names)
+labels = np.load('arrays/labels.npy')
+coord = np.load('arrays/coord_all_interactions.npy')
 
 # Preprocess data (zero centered and between -1 and 1)
 coord -= 0.5
 coord *= 2
 
-print("coord", coord.shape)
+# Slicing the data
+keep = 100
+labels = labels[0:keep]
+coord = coord[0:keep]
 
-sys.exit()
-
+print("----Setting up the model----")
 # Setting hyperparameters
 reg = 0.0
-drop = 0.5
+drop = 0.0
 
 # Setting up model
 model = Sequential()
-model.add(Dense(64, input_dim=4, activation='relu',kernel_regularizer=regularizers.l2(reg)))
+model.add(Dense(64, input_dim=8, activation='relu',kernel_regularizer=regularizers.l2(reg)))
 model.add(Dropout(drop))
 model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(reg)))
 model.add(Dropout(drop))
@@ -41,8 +42,9 @@ model.add(Dense(1, activation='sigmoid'))
 # Compiling the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+print("----Model training----")
 # Model training
-model.fit(coord, labels, epochs=10, batch_size=128)
+model.fit(coord, labels, epochs=1, batch_size=64)
 
 #score = model.evaluate(x_test, y_test, batch_size=128)
 #print(score)
