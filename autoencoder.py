3db347bdec9df3295ae482e4e07803bc607a7f46
ld2113 <ld2113@ic.ac.uk>
2017-05-29 15:36:41 +0100

    Testing autoencoder (added metrics)

--- autoencoder.py
+++ autoencoder.py
@@ -1,3 +1,4 @@
+print("in, 1, out")
 import keras
 from keras.layers import Input, Dense
 from keras.models import Model
@@ -12,11 +13,11 @@ train = pd.read_pickle('arrays/domain_df.pckl').values
 
 
 input = Input(shape=(train.shape[1],))
-encoded = Dense(1024, activation='relu')(input)
-#encoded = Dense(2048, activation='relu')(encoded)
+encoded = Dense(1, activation='relu')(input)
+#encoded = Dense(1, activation='relu')(encoded)
 #encoded = Dense(1024, activation='relu')(encoded)
 
-#decoded = Dense(2048, activation='relu')(encoded)
+#decoded = Dense(128, activation='relu')(encoded)
 #decoded = Dense(4096, activation='relu')(decoded)
 decoded = Dense(train.shape[1], activation='sigmoid')(encoded)
 
@@ -26,14 +27,21 @@ autoencoder = Model(input, decoded)
 adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 autoencoder.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
 
+cb = []
+cb.append(keras.callbacks.EarlyStopping(monitor='loss', min_delta=0, patience=2, verbose=1, mode='auto'))
+cb.append(keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.3, patience=2, verbose=1, mode='auto', epsilon=1e-07, cooldown=0, min_lr=0))
+
 autoencoder.fit(train, train,
-                epochs=100,
+                epochs=4000,
                 batch_size=64,
                 shuffle=True,
-                validation_split=0.25)
+                callbacks=cb)
 
 print("INSUM",np.sum(train))
-bin_out = (autoencoder.predict(train) > 0.5).astype(int)
+pred_out = autoencoder.predict(train)
+bin_out = (pred_out > 0.5).astype(int)
 print("OUTSUM",np.sum(bin_out))
-
-#print("AUROC", met.roc_auc_score(labels_test, epoch_pred))
+print("Accuracy:", met.accuracy_score(train, bin_out))
+print("Precision:", met.precision_score(train, bin_out, average='micro'))
+print("AUROC:", met.roc_auc_score(train, pred_out, average='micro'))
+print("AUPRC:", met.average_precision_score(train, pred_out, average='micro'))
