a84b584926847b67508000d8922c8937a2d7b228
ld2113 <ld2113@ic.ac.uk>
2017-05-30 19:36:08 +0100

    Autoencoder tweaking and todo list

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # Data-Analysis-Project
 
 ## ToDo
-- Keras
+- Fix generator (without SMOTE)
+- Do random sampling in generator instead of SMOTE (compare methods)
+- Include automatic NN metric logging
+- Compare performance of different autoencoder outputs
+- How to do partial embedding for test and validation data
+- Create network with two seperate inputs which are later joined
+- Is it actually okay to train the autoencoder without holdout?
+- use 4/7/10D embedding
+- Pass val. data directly to network (rather then testing later)
 
-- Udacity Data Visualisation Course
-- D3
-- HTML Templates, CSS etc...
+- Use crossvalidation to validate NN sklearn.cross_validation.StratifiedKfold
+- Find way to replace embedding
+
+- Website
+-- viz: different network structures and their performance - interactive
+-- viz: subsample embedding
 
 ## Questions
--- autoencoder.py
+++ autoencoder.py
@@ -1,4 +1,4 @@
-print("in, 1, out")
+print("in, 20, out")
 import keras
 from keras.layers import Input, Dense
 from keras.models import Model
@@ -11,9 +11,12 @@ import sklearn.metrics as met
 
 train = pd.read_pickle('arrays/domain_df.pckl').values
 
+#train = data[0:int(0.8*len(data))]
+#test = data[int(0.8*len(data)):]
+
 
 input = Input(shape=(train.shape[1],))
-encoded = Dense(1, activation='relu')(input)
+encoded = Dense(20, activation='tanh')(input)
 #encoded = Dense(1, activation='relu')(encoded)
 #encoded = Dense(1024, activation='relu')(encoded)
 
@@ -29,7 +32,7 @@ autoencoder.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accura
 
 cb = []
 cb.append(keras.callbacks.EarlyStopping(monitor='loss', min_delta=0, patience=2, verbose=1, mode='auto'))
-cb.append(keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.3, patience=2, verbose=1, mode='auto', epsilon=1e-07, cooldown=0, min_lr=0))
+cb.append(keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.3, patience=2, verbose=1, mode='auto', epsilon=1e-08, cooldown=0, min_lr=0))
 
 autoencoder.fit(train, train,
                 epochs=4000,
@@ -37,6 +40,7 @@ autoencoder.fit(train, train,
                 shuffle=True,
                 callbacks=cb)
 
+print("TRAIN STATISTICS")
 print("INSUM",np.sum(train))
 pred_out = autoencoder.predict(train)
 bin_out = (pred_out > 0.5).astype(int)
@@ -45,3 +49,14 @@ print("Accuracy:", met.accuracy_score(train, bin_out))
 print("Precision:", met.precision_score(train, bin_out, average='micro'))
 print("AUROC:", met.roc_auc_score(train, pred_out, average='micro'))
 print("AUPRC:", met.average_precision_score(train, pred_out, average='micro'))
+'''
+print("TEST STATISTICS")
+print("INSUM",np.sum(test))
+pred_test = autoencoder.predict(test)
+bin_test = (pred_test > 0.5).astype(int)
+print("OUTSUM",np.sum(bin_test))
+print("Accuracy:", met.accuracy_score(test, bin_test))
+print("Precision:", met.precision_score(test, bin_test, average='micro'))
+print("AUROC:", met.roc_auc_score(test, pred_test, average='micro'))
+print("AUPRC:", met.average_precision_score(test, pred_test, average='micro'))
+'''
