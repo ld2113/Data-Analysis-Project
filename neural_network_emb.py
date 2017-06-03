197ca0a1da6fba0d26d67cc806c7dc984f121de4
ld2113 <ld2113@ic.ac.uk>
2017-06-03 23:43:41 +0100

    Add automatic data logging, batchnorm, restructuring

--- neural_network_emb.py
+++ neural_network_emb.py
@@ -7,6 +7,7 @@ import pandas as pd
 import sys
 import pickle
 import copy
+import time
 
 import sklearn.metrics as met
 from multiprocessing_generator import ParallelGenerator
@@ -14,7 +15,7 @@ from multiprocessing_generator import ParallelGenerator
 import keras
 from keras import regularizers
 from keras.models import Sequential, Model
-from keras.layers import Input, Dense, Dropout, Embedding
+from keras.layers import Input, Dense, Dropout, Activation, Embedding, Flatten, BatchNormalization
 
 
 class Cust_metrics(keras.callbacks.Callback):
@@ -29,14 +30,24 @@ class Cust_metrics(keras.callbacks.Callback):
 
 	def on_epoch_end(self, epoch, logs={}):
 
-		epoch_pred = self.model.predict(test_lab_names[0:2])
+		epoch_pred = self.model.predict([test_lab_names[:,0:2],test_lab_names[:,0:2]], batch_size=batchsize, verbose=1)
 		epoch_bin_pred = np.array(epoch_pred > 0.5).astype(int)
 		labels_test = test_lab_names[0:epoch_pred.shape[0],2]
 
-		print('\n', 'Custom epoch metrics:  auroc:', met.roc_auc_score(labels_test, epoch_pred), ' - aupr:', met.average_precision_score(labels_test, epoch_pred), end=' - ')
-		print('mcc:', met.matthews_corrcoef(labels_test,epoch_bin_pred), ' - acc:', met.accuracy_score(labels_test,epoch_bin_pred), end=' - ')
-		print("Prediciton  MAX:", max(epoch_pred) , "MIN:", min(epoch_pred), end=' - ')
-		print("Frac. pos. pred.:", np.sum(epoch_bin_pred)/len(epoch_bin_pred))
+		acc = met.accuracy_score(labels_test,epoch_bin_pred)
+		prec = met.precision_score(labels_test,epoch_bin_pred)
+		mcc = met.matthews_corrcoef(labels_test,epoch_bin_pred)
+		auroc = met.roc_auc_score(labels_test, epoch_pred)
+		aupr = met.average_precision_score(labels_test, epoch_pred)
+		frac_pos = np.sum(epoch_bin_pred)/len(epoch_bin_pred)
+
+		print('\n', 'auroc:', auroc, ' - aupr:', aupr, end=' - ')
+		print('mcc:', mcc, ' - acc:', acc, ' - prec:', prec, end=' - ')
+		print("Frac. pos. pred.:", frac_pos)
+
+		with open('log.csv', 'a') as f:
+			f.write(','.join(list(map(lambda x: str.replace(x, ",", ";"),list(map(str,[id,time.strftime('%Y%m%d'),time.strftime('%H%M'),epoch+1,max_epochs,logs['loss'],logs['acc'],mcc,acc,prec,auroc,aupr,frac_pos,
+			input_mode,coord_struct,dom_struct,concat_struct,reg,drop,batchsize,act,optim,lr,lr_sched,lr_plat,train_path,test_path,coord_path,dom_path,coord_norm,aux_output_weights,'\n']))))))
 
 		return
 
@@ -48,7 +59,7 @@ class Cust_metrics(keras.callbacks.Callback):
 
 
 ################################ Functions #####################################
-'''
+
 def normalise_coord(array_list):
 
 	for i in range(len(array_list)):
@@ -58,87 +69,84 @@ def normalise_coord(array_list):
 	return array_list
 
 
-def subset_data(labels, coord, keep=1.0, train_split=1.0):
+def build_model(input_mode, coord_struct, dom_struct, concat_struct, coord_embedding, dom_embedding):
 
-	#Subsetting data
-	labels = labels[0:int(keep*len(labels))]
-	coord = coord[0:int(keep*len(coord))]
+	if input_mode == 'cd':
+		coord_in = Input(shape=(2,), dtype='int32', name='coord_in')
+		coord_net = Embedding(output_dim=coord_embedding.shape[1],input_dim=coord_embedding.shape[0],weights=[coord_embedding],input_length=2,trainable=False)(coord_in)
+		coord_net = Flatten()(coord_net)
+		for n in coord_struct:
+			coord_net = Dense(n, kernel_regularizer=regularizers.l2(reg), kernel_initializer='he_normal')(coord_net)
+			coord_net = BatchNormalization()(coord_net)
+			coord_net = Activation(act)(coord_net)
+			coord_net = Dropout(drop)(coord_net)
 
-	# Splitting data
-	labels_train = labels[0:int(len(labels)*train_split)]
-	coord_train = coord[0:int(len(coord)*train_split)]
-	labels_test = labels[int(len(labels)*train_split):]
-	coord_test = coord[int(len(coord)*train_split):]
+		dom_in = Input(shape=(2,), dtype='int32', name='dom_in')
+		dom_net = Embedding(output_dim=dom_embedding.shape[1],input_dim=dom_embedding.shape[0],weights=[dom_embedding],input_length=2,trainable=False)(dom_in)
+		dom_net = Flatten()(dom_net)
+		for n in dom_struct:
+			dom_net = Dense(n, activation=act, kernel_regularizer=regularizers.l2(reg), kernel_initializer='he_normal')(dom_net)
+			dom_net = BatchNormalization()(dom_net)
+			dom_net = Activation(act)(dom_net)
+			dom_net = Dropout(drop)(dom_net)
 
-	return labels_train, labels_test, coord_train, coord_test
-'''
+		x = keras.layers.concatenate([coord_net, dom_net])
+		for n in concat_struct:
+			x = Dense(n, activation=act, kernel_regularizer=regularizers.l2(reg), kernel_initializer='he_normal')(x)
+			x = BatchNormalization()(x)
+			x = Activation(act)(x)
+			x = Dropout(drop)(x)
 
-'''
+		output = Dense(1, activation='sigmoid', name='output')(x)
 
-def test_gen(test_lab_names, coord_df, domains_df, b_size):
-	c = 0
-	while True:
-		coord1 = coord_df.loc[test_lab_names[c*b_size:(c+1)*b_size,0]].values
-		domains1 = domains_df.loc[test_lab_names[c*b_size:(c+1)*b_size,0]].values
-		coord2 = coord_df.loc[test_lab_names[c*b_size:(c+1)*b_size,1]].values
-		domains2 = domains_df.loc[test_lab_names[c*b_size:(c+1)*b_size,1]].values
+		model = Model(inputs=[coord_in, dom_in], outputs=output)
 
-		#b_data = np.concatenate((coord1, coord2, domains1, domains2), axis=1)
-		b_coord = np.concatenate((coord1, coord2), axis=1)
-		b_domains = np.concatenate((domains1, domains2), axis=1)
-		b_labels = test_lab_names[c*b_size:(c+1)*b_size,2]
+	elif input_mode == 'c':
+		coord_in = Input(shape=(2,), dtype='int32', name='coord_in')
+		coord_net = Embedding(output_dim=coord_embedding.shape[1],input_dim=coord_embedding.shape[0],weights=[coord_embedding],input_length=2,trainable=False)(coord_in)
+		coord_net = Flatten()(coord_net)
+		for n in coord_struct:
+			coord_net = Dense(n, activation=act, kernel_regularizer=regularizers.l2(reg), kernel_initializer='he_normal')(coord_net)
+			coord_net = BatchNormalization()(coord_net)
+			coord_net = Activation(act)(coord_net)
+			coord_net = Dropout(drop)(coord_net)
+		output = Dense(1, activation='sigmoid', name='output')(coord_net)
 
-		c += 1
+		model = Model(inputs=coord_in, outputs=output)
 
-		yield ([b_coord,b_domains],b_labels)
+	elif input_mode == 'd':
+		dom_in = Input(shape=(2,), dtype='int32', name='dom_in')
+		dom_net = Embedding(output_dim=dom_embedding.shape[1],input_dim=dom_embedding.shape[0],weights=[dom_embedding],input_length=2,trainable=False)(dom_in)
+		dom_net = Flatten()(dom_net)
+		for n in dom_struct:
+			dom_net = Dense(n, activation=act, kernel_regularizer=regularizers.l2(reg), kernel_initializer='he_normal')(dom_net)
+			dom_net = BatchNormalization()(dom_net)
+			dom_net = Activation(act)(dom_net)
+			dom_net = Dropout(drop)(dom_net)
+		output = Dense(1, activation='sigmoid', name='output')(dom_net)
 
+		model = Model(inputs=dom_in, outputs=output)
 
-def train_gen(lab_names_pos, lab_names_neg, coord_df, domains_df, b_size):
-	while True:
-		pos_sample = lab_names_pos[np.random.choice(lab_names_pos.shape[0], size=int(b_size/2), replace=False)]
-		neg_sample = lab_names_neg[np.random.randint(lab_names_neg.shape[0], size=int(b_size/2))]
+	return model
 
-		lab_names_batch = np.concatenate((pos_sample, neg_sample), axis=0)
-		np.random.shuffle(lab_names_batch)
-
-		coord1 = coord_df.loc[lab_names_batch[:,0]].values
-		domains1 = domains_df.loc[lab_names_batch[:,0]].values
-		coord2 = coord_df.loc[lab_names_batch[:,1]].values
-		domains2 = domains_df.loc[lab_names_batch[:,1]].values
-
-		b_coord = np.concatenate((coord1, coord2), axis=1)
-		b_domains = np.concatenate((domains1, domains2), axis=1)
-		b_labels = lab_names_batch[:,2]
-
-		yield ([b_coord,b_domains],b_labels)
-
-'''
 
 def set_callbacks():
-
 	cb = []
-
-	#cb.append(keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=3, verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0))
-	#cb.append(keras.callbacks.LearningRateScheduler(schedule))
-	#cb.append(keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.00001, patience=10, verbose=1, mode='auto'))
 	cb.append(Cust_metrics())
+
+	if lr_sched == 'pow2':
+		def schedule(epoch):
+			return lr/pow(2,epoch)
+		cb.append(keras.callbacks.LearningRateScheduler(schedule))
+
+	elif lr_plat != []:
+		cb.append(keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=lr_plat[0], patience=lr_plat[1], verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0))
+
+	#cb.append(keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.00001, patience=10, verbose=1, mode='auto'))
 	#cb.append(keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=1, write_graph=True, write_images=True))
 
 	return cb
 
-
-def schedule(epoch):
-	return 0.0003/pow(2,epoch)
-
-
-def load(names):
-	coord_df = pd.DataFrame(np.load('arrays/090_embed/4D_coord_4x16k.npy'), index=names)
-
-	train_lab_names_pos = np.load('arrays/090_embed/nl_pos_train_01_emb.npy')
-	train_lab_names_neg = np.load('arrays/090_embed/nl_neg_train_01_emb.npy')
-	test_lab_names = np.load('arrays/090_embed/nl_test_01_emb.npy')
-
-	return coord_df, train_lab_names_pos, train_lab_names_neg, test_lab_names
 
 
 
@@ -147,65 +155,67 @@ def load(names):
 # Setting hyperparameters
 reg = 0.0
 drop = 0.0
-nlayers = 1
-nunits = 138
+optim = 'adam' # 'adam'-> default lr:0.001; 'sgd'-> default lr:0.01
 batchsize = 64
 act = 'relu'
+coord_norm = 'man' # 'man'-> -0.5,*2; 'mean'-> -mean, /std; None
+max_epochs = 5
+
+lr = 0.001
+lr_sched = 'pow2' # 'pow2'-> lr/pow(2,epoch)
+lr_plat = [] # ReduceLROnPlateau: [factor, patience]
+
+dom_path = 'arrays/encoded_dom_20_relu_1.npy'
+coord_path = 'arrays/090_embed/4D_coord_4x16k.npy'
+train_path = 'arrays/090_embed/nl_train_res_01_emb.npy'
+test_path = 'arrays/090_embed/nl_test_01_emb.npy'
+
+input_mode = 'cd' # 'c'-> coordinates only; 'd'-> domains only; 'cd'-> both
+coord_struct = [128,1]
+dom_struct = [128,1]
+concat_struct = []
+aux_output_weights = []
+
+with open('counter.txt', 'r') as f:
+	id = int(f.readline()) + 1
+with open('counter.txt', 'w') as f:
+	f.write(str(id))
 
 
 ################################################################
 print("----Loading Data----")
 
-names = np.load('arrays/names.npy')
-domains_df = pd.DataFrame(np.load('arrays/encoded_dom_20_relu_1.npy'), index=names)
+dom_embedding = np.load(dom_path)
 
-#Make sure that right dataset is loading (full or partial embed)!
-coord_df, train_lab_names_pos, train_lab_names_neg, test_lab_names = load(names)
+coord_embedding = np.load(coord_path)
+test_lab_names = np.load(test_path)
+train_lab_names = np.load(train_path)
 
-coord_embedding = coord_df.values
-dom_embedding = domains_df.values
 
 ################################################################
 print("----Processing Data----")
 
-coord_df -= 0.5
-coord_df *= 2.0
+if coord_norm == 'man':
+	coord_embedding -= 0.5
+	coord_embedding *= 2.0
 
-iterations = int(np.ceil((len(train_lab_names_neg)-len(train_lab_names_pos)) / len(train_lab_names_pos)))
-for i in range(iterations):
-	if i==0:
-		train_lab_emb_pos = copy.copy(train_lab_names_pos)
-	train_lab_emb_pos = np.concatenate((train_lab_emb_pos,train_lab_names_pos), axis=0)
-
-train_lab_emb = np.concatenate((train_lab_names_neg,train_lab_emb_pos[0:len(train_lab_names_neg)]), axis=0)
-
-np.random.shuffle(train_lab_emb)
-
+elif coord_norm == 'mean':
+	coord_embedding = normalise_coord([coord_embedding])[0]
 
 ################################################################
 print("----Setting up the model----")
 
-coord_in = Input(shape=(2,), dtype='int32', name='coord_in')
-coord_net = Embedding(output_dim=coord_embedding.shape[1],input_dim=coord_embedding.shape[0],weights=[coord_embedding],input_length=2,trainable=False)(coord_in)
-coord_net = Dense(128, activation=act)(coord_net)
-
-dom_in = Input(shape=(2,), name='dom_in')
-dom_net = Embedding(output_dim=dom_embedding.shape[1],input_dim=dom_embedding.shape[0],weights=[dom_embedding],input_length=2,trainable=False)(dom_in)
-dom_net = Dense(128, activation=act)(dom_net)
-
-x = keras.layers.concatenate([coord_net, dom_net])
-x = Dense(128, activation=act)(x)
-
-output = Dense(1, activation='sigmoid', name='output')(x)
-
-model = Model(inputs=[coord_in, dom_in], outputs=output)
+model = build_model(input_mode, coord_struct, dom_struct, concat_struct, coord_embedding, dom_embedding)
 
 
 ################################################################
 print("----Compiling the model----")
 
-opt = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-#opt = keras.optimizers.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=True)
+if optim == 'adam':
+	opt = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+elif optim == 'sgd':
+	opt = keras.optimizers.SGD(lr=lr, momentum=0.0, decay=0.0, nesterov=True)
 
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
@@ -213,4 +223,4 @@ model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 ################################################################
 print("----Model training----")
 
-model.fit([train_lab_emb[0:2], train_lab_emb[0:2]], train_lab_emb[2:3], epochs=5, batch_size=64, callbacks=set_callbacks())
+model.fit([train_lab_names[:,0:2], train_lab_names[:,0:2]], train_lab_names[:,2], epochs=max_epochs, batch_size=batchsize, callbacks=set_callbacks())
