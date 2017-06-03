0a9aa2df26182dda06b851459331d5422566fbff
ld2113 <ld2113@ic.ac.uk>
2017-06-03 23:40:20 +0100

    Add oversampling from training set

--- preprocessing/6_create_indices.py
+++ preprocessing/6_create_indices.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 
 import numpy as np
 import pickle
+import copy
 
 
 def indexify(list, dic):
@@ -14,17 +15,36 @@ def indexify(list, dic):
 	return list
 
 
+def resample(train_lab_names_neg, train_lab_names_pos):
+
+	iterations = int(np.ceil((len(train_lab_names_neg)-len(train_lab_names_pos)) / len(train_lab_names_pos)))
+	for i in range(iterations):
+		if i==0:
+			train_lab_emb_pos = copy.copy(train_lab_names_pos)
+		train_lab_emb_pos = np.concatenate((train_lab_emb_pos,train_lab_names_pos), axis=0)
+
+	train_lab_emb = np.concatenate((train_lab_names_neg,train_lab_emb_pos[0:len(train_lab_names_neg)]), axis=0)
+
+	np.random.shuffle(train_lab_emb)
+
+	return train_lab_emb
+
+
 ###### Load Data #######
-train_lab_names_pos = np.load('arrays/090_embed/nl_pos_train_01.npy')
-train_lab_names_neg = np.load('arrays/090_embed/nl_neg_train_01.npy')
-test_lab_names = np.load('arrays/090_embed/nl_test_01.npy')
-dic = pickle.load(open('arrays/inverse_dic.pkl','rb'))
+train_lab_names_pos = np.load('arrays/090_embed/nl_pos_train_01_emb.npy')
+train_lab_names_neg = np.load('arrays/090_embed/nl_neg_train_01_emb.npy')
+#test_lab_names = np.load('arrays/090_embed/nl_test_01.npy')
+#dic = pickle.load(open('arrays/inverse_dic.pkl','rb'))
 
 ###### Process Data ######
-list = [train_lab_names_pos,train_lab_names_neg,test_lab_names]
-list = indexify(list, dic)
+#list = [train_lab_names_pos,train_lab_names_neg,test_lab_names]
+#list = indexify(list, dic)
+
+train_lab_emb = resample(train_lab_names_neg, train_lab_names_pos)
 
 ###### Save Data ######
-np.save('arrays/090_embed/nl_pos_train_01_emb.npy',list[0].astype(int))
-np.save('arrays/090_embed/nl_neg_train_01_emb.npy',list[1].astype(int))
-np.save('arrays/090_embed/nl_test_01_emb.npy',list[2].astype(int))
+#np.save('arrays/090_embed/nl_pos_train_01_emb.npy',list[0].astype(int))
+#np.save('arrays/090_embed/nl_neg_train_01_emb.npy',list[1].astype(int))
+#np.save('arrays/090_embed/nl_test_01_emb.npy',list[2].astype(int))
+#np.save('arrays/090_embed/nl_test_01_emb.npy',list[2].astype(int))
+np.save('arrays/090_embed/nl_train_res_01_emb.npy',train_lab_emb)
