c86a720b205e11784825a836f65e75ad6a69c313
ld2113 <ld2113@ic.ac.uk>
2017-06-13 19:14:03 +0100

    Fix data logging issue

--- autoencoder.py
+++ autoencoder.py
@@ -18,64 +18,6 @@ from keras.layers import Input, Dense, Dropout, Activation, Embedding, Flatten, 
 
 def main(reg, drop, optim, batchsize, act, max_epochs, lr, train_split, lr_sched, lr_plat,
 		in_path, save_path, struct):
-
-	class Cust_metrics(keras.callbacks.Callback):
-		def on_train_begin(self, logs={}):
-			return
-
-		def on_train_end(self, logs={}):
-
-			pred_train = self.model.predict(train, batch_size=batchsize, verbose=1)
-			pred_train_bin = np.array(pred_train > 0.5).astype(int)
-
-			insum_train = np.sum(train)
-
-			outsum_train = np.sum(pred_train_bin)
-			acc_train = met.accuracy_score(train,pred_train_bin)
-			prec_train = met.precision_score(train, pred_train_bin, average='micro')
-			recall_train = met.recall_score(train, pred_train_bin, average='micro')
-			f1_train = met.f1_score(train, pred_train_bin, average='micro')
-			auroc_train = met.roc_auc_score(train, pred_train, average='micro')
-			aupr_train = met.average_precision_score(train, pred_train, average='micro')
-
-			if train_split != 1.0:
-				pred_test = self.model.predict(test, batch_size=batchsize, verbose=1)
-				pred_test_bin = np.array(pred_test > 0.5).astype(int)
-
-				insum_test = np.sum(test)
-
-				outsum_test = np.sum(pred_test_bin)
-				acc_test = met.accuracy_score(test,pred_test_bin)
-				prec_test = met.precision_score(test, pred_test_bin, average='micro')
-				recall_test = met.recall_score(test, pred_test_bin, average='micro')
-				f1_test = met.f1_score(test, pred_test_bin, average='micro')
-				auroc_test = met.roc_auc_score(test, pred_test, average='micro')
-				aupr_test = met.average_precision_score(test, pred_test, average='micro')
-
-				print('\n', 'insum_train:', insum_train, ' - outsum_train:', outsum_train, ' - acc_train:', acc_train, ' - prec_train:', prec_train, end=' - ')
-				print('recall_train:', recall_train, ' - f1_train:', f1_train, ' - auroc_train:', auroc_train, ' - aupr_train:', aupr_train)
-
-				print('\n', 'insum_test:', insum_test, ' - outsum_test:', outsum_test, ' - acc_test:', acc_test, ' - prec_test:', prec_test, end=' - ')
-				print('recall_test:', recall_test, ' - f1_test:', f1_test, ' - auroc_test:', auroc_test, ' - aupr_test:', aupr_test)
-
-			else:
-				outsum_test = 'n/a'
-				acc_test = 'n/a'
-				prec_test = 'n/a'
-				recall_test = 'n/a'
-				f1_test = 'n/a'
-				auroc_test = 'n/a'
-				aupr_test = 'n/a'
-
-				print('\n', 'insum_train:', insum, ' - outsum_train:', outsum_train, ' - acc_train:', acc_train, ' - prec_train:', prec_train, end=' - ')
-				print('recall_train:', recall_train, ' - f1_train:', f1_train, ' - auroc_train:', auroc_train, ' - aupr_train:', aupr_train)
-
-			with open('log_autoenc.csv', 'a') as f:
-				f.write(','.join(list(map(lambda x: str.replace(x, ",", ";"),list(map(str,[id,time.strftime('%Y%m%d'),time.strftime('%H%M'), max_epochs, max_epochs,
-				train_split, "n/a","n/a", insum_train, outsum_train, acc_train, prec_train, recall_train, f1_train, auroc_train, aupr_train, insum_test, outsum_test, acc_test, prec_test, recall_test, f1_test, auroc_test, aupr_test,
-				struct,reg,drop,batchsize,act,optim,lr,lr_sched,lr_plat,in_path,save_path,'\n']))))))
-
-
 			return
 
 		def on_epoch_begin(self, epoch, logs={}):
@@ -83,7 +25,7 @@ def main(reg, drop, optim, batchsize, act, max_epochs, lr, train_split, lr_sched
 
 		def on_epoch_end(self, epoch, logs={}):
 
-			if epoch%100 == 0 and epoch != 0:
+			if (epoch+1)%100 == 0 and epoch != 0:
 				pred_train = self.model.predict(train, batch_size=batchsize, verbose=1)
 				pred_train_bin = np.array(pred_train > 0.5).astype(int)
 
@@ -130,7 +72,7 @@ def main(reg, drop, optim, batchsize, act, max_epochs, lr, train_split, lr_sched
 					print('recall_train:', recall_train, ' - f1_train:', f1_train, ' - auroc_train:', auroc_train, ' - aupr_train:', aupr_train)
 
 				with open('log_autoenc.csv', 'a') as f:
-					f.write(','.join(list(map(lambda x: str.replace(x, ",", ";"),list(map(str,[id,time.strftime('%Y%m%d'),time.strftime('%H%M'), epoch, max_epochs,
+					f.write(','.join(list(map(lambda x: str.replace(x, ",", ";"),list(map(str,[id,time.strftime('%Y%m%d'),time.strftime('%H%M'), epoch+1, max_epochs,
 					train_split, logs['loss'], logs['acc'], insum_train, outsum_train, acc_train, prec_train, recall_train, f1_train, auroc_train, aupr_train, insum_test, outsum_test, acc_test, prec_test, recall_test, f1_test, auroc_test, aupr_test,
 					struct,reg,drop,batchsize,act,optim,lr,lr_sched,lr_plat,in_path,save_path,'\n']))))))
 
