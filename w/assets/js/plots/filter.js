bf9c2273a868ceed07d94d3c79aa76b49a99ee8b
ld2113 <ld2113@ic.ac.uk>
2017-06-15 18:46:16 +0100

    Working on Website

--- w/assets/js/plots/filter.js
+++ w/assets/js/plots/filter.js
@@ -1,82 +1,14 @@
 d3.csv("logwebsite.csv", function(error, data) {
 
-	data.forEach(function(d) {
-		d.epoch = +d.epoch;
-		d.mcc = +d.mcc;
-		});
+	function makesvg(plotname) {
+		var plot = d3.select("#discussion").append("svg")
+			.attr("id", plotname)
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var margin = {top: 30, right: 20, bottom: 50, left: 70},
-		width = 0.25 * window.innerWidth - margin.left - margin.right,
-		height = width - margin.top - margin.bottom;
-
-	var x = d3.scale.linear()
-		.range([0, width]);
-
-	var y = d3.scale.linear()
-		.range([height, 0]);
-
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom")
-		.tickFormat(d3.format("d"))
-		.ticks(5);
-
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left");
-
-	var mccplot = d3.select("#discussion").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	var f1plot = d3.select("#discussion").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-	// Helper function to add new points to our data
-	function enterPoints(data, plot, yvar) {
-		// Add the points!
-		plot.selectAll(".point")
-			.data(data)
-			.enter().append("path")
-			.attr("class", "point")
-			.attr('fill', 'white')
-			.attr("d", d3.svg.symbol().type("circle"))
-			.attr("transform", function(d) { return "translate(0," + height + ")"; })
-			.transition()
-			.duration(1000)
-			.attr("transform", function(d) { return "translate(" + x(d.epoch) + "," + y(eval("d."+yvar)) + ")"; });
-
-	}
-
-	function exitPoints(data, plot) {
-		plot.selectAll(".point")
-			.data(data)
-			.exit()
-			.transition()
-			.duration(1000)
-			.attr("transform", function(d) { return "translate(0," + height + ")"; })
-			.remove();
-	}
-
-	function updatePoints(data, plot, yvar) {
-		x.domain([0, d3.max(data, function(d) { return d.epoch; })]).nice();
-
-		plot.selectAll(".point")
-			.data(data)
-			.transition()
-			.duration(1000)
-			.attr("transform", function(d) { return "translate(" + x(d.epoch) + "," + y(eval("d."+yvar)) + ")"; });
-
-		plot.select(".x.axis")
-			.transition()
-			.duration(1000)
-			.call(xAxis);
+		return plot
 	}
 
 	function createAxis(data, plot, txt, yvar) {
@@ -119,31 +51,116 @@ d3.csv("logwebsite.csv", function(error, data) {
 			.text(txt);
 	}
 
+	function enterPoints(data, plot, yvar) {
+		y.domain([0, d3.max(data, function(d) { return eval("d."+yvar); })]).nice();
+		// Add the points!
+		plot.selectAll(".point")
+			.data(data)
+			.enter().append("path")
+			.attr("class", "point")
+			.attr('fill', 'white')
+			.attr("d", d3.svg.symbol().type("circle"))
+			.attr("transform", function(d) { return "translate(0," + height + ")"; })
+			.transition()
+			.duration(1000)
+			.attr("transform", function(d) { return "translate(" + x(d.epoch) + "," + y(eval("d."+yvar)) + ")"; });
+	}
+
+	function exitPoints(data, plot) {
+		plot.selectAll(".point")
+			.data(data)
+			.exit()
+			.transition()
+			.duration(1000)
+			.attr("transform", function(d) { return "translate(0," + height + ")"; })
+			.remove();
+	}
+
+	function updatePoints(data, plot, yvar) {
+		x.domain([0, d3.max(data, function(d) { return d.epoch; })]).nice();
+
+		plot.selectAll(".point")
+			.data(data)
+			.transition()
+			.duration(1000)
+			.attr("transform", function(d) { return "translate(" + x(d.epoch) + "," + y(eval("d."+yvar)) + ")"; });
+
+		plot.select(".x.axis")
+			.transition()
+			.duration(1000)
+			.call(xAxis);
+	}
+
 	function getFilteredData(data, optim, input_mode, class_weights) {
 		return data.filter(function(point) { return point.class_weight === class_weights; })
 					.filter(function(point) { return point.input_mode === input_mode; })
 					.filter(function(point) { return point.optim === optim; });
 	}
 
-	// New select element for allowing the user to select a group!
-	var $class_weightsSelector = document.querySelector('#classweights-select');
+
+////////////////////////////////// MAIN PROGRAM ////////////////////////////////
+
+	document.getElementById("nnform").reset();
+
+	data.forEach(function(d) {
+		d.epoch = +d.epoch;
+		d.mcc = +d.mcc;
+		d.f1 = +d.f1;
+		d.prec = +d.prec;
+		d.recall = +d.recall;
+		});
+
+	var margin = {top: 30, right: 50, bottom: 50, left: 70},
+		width = 0.25 * window.innerWidth - margin.left - margin.right,
+		height = width - margin.top - margin.bottom;
+
+	var x = d3.scale.linear()
+		.range([0, width]);
+
+	var y = d3.scale.linear()
+		.range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom")
+		.tickFormat(d3.format("d"))
+		.ticks(5);
+
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left");
+
+	var class_weights = d3.select("#classweights-select").node().value;
 	var inmodbutt = d3.select('input[name="input-mode"]:checked').property("value");
 	var optimbutt = d3.select('input[name="optim"]:checked').property("value");
-	var groupData = getFilteredData(data, optimbutt, inmodbutt, $class_weightsSelector.value);
 
-	// Compute the scales’ domains.
+	var groupData = getFilteredData(data, optimbutt, inmodbutt, class_weights);
+
+	var mccplot = makesvg("mccplot")
+	var f1plot = makesvg("f1plot")
+	d3.select("#discussion").append("br")
+	d3.select("#discussion").append("br")
+	var precplot = makesvg("precplot")
+	var recplot = makesvg("recplot")
+
 	x.domain([0, d3.max(groupData, function(d) { return d.epoch; })]).nice();
 
-	createAxis(groupData, mccplot, "Matthews Correlation Coefficient", "mcc")
-	createAxis(groupData, f1plot, "F1 Score", "f1")
+	createAxis(data, mccplot, "Matthews Correlation Coefficient", "mcc");
+	createAxis(data, f1plot, "F1 Score", "f1");
+	createAxis(data, recplot, "Recall", "recall");
+	createAxis(data, precplot, "Precision", "prec");
 
 	enterPoints(groupData, mccplot, "mcc");
 	enterPoints(groupData, f1plot, "f1");
+	enterPoints(groupData, precplot, "prec");
+	enterPoints(groupData, recplot, "recall");
 
-	$class_weightsSelector.onchange = function(e) {
-		var class_weights = e.target.value;
+
+	d3.selectAll("input,select")
+		.on("change", function(e) {
+		var class_weights = d3.select("#classweights-select").node().value;
 		var input_mode = d3.select('input[name="input-mode"]:checked').property("value");
-		optim = d3.select('input[name="optim"]:checked').property("value");
+		var optim = d3.select('input[name="optim"]:checked').property("value");
 		var groupData = getFilteredData(data, optim, input_mode, class_weights);
 
 		updatePoints(groupData, mccplot, "mcc");
@@ -154,22 +171,13 @@ d3.csv("logwebsite.csv", function(error, data) {
 		enterPoints(groupData, f1plot, "f1");
 		exitPoints(groupData, f1plot);
 
-	};
+		updatePoints(groupData, precplot, "prec");
+		enterPoints(groupData, precplot, "prec");
+		exitPoints(groupData, precplot);
 
-	d3.selectAll("input")
-		.on("change", function(e) {
-		var class_weights = $class_weightsSelector.value;
-		var input_mode = d3.select('input[name="input-mode"]:checked').property("value");
-		optim = d3.select('input[name="optim"]:checked').property("value");
-		var groupData = getFilteredData(data, optim, input_mode, class_weights);
-
-		updatePoints(groupData, mccplot, "f1");
-		enterPoints(groupData, mccplot, "f1");
-		exitPoints(groupData, mccplot);
-
-		updatePoints(groupData, f1plot, "f1");
-		enterPoints(groupData, f1plot, "f1");
-		exitPoints(groupData, f1plot);
+		updatePoints(groupData, recplot, "recall");
+		enterPoints(groupData, recplot, "recall");
+		exitPoints(groupData, recplot);
 
 	});
 	/*
